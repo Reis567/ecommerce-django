5895e579130606b3e78d3bb96c32d5797ec96044
Reis567 <matheusdosreis9@gmail.com>
2023-10-14 18:07:15 -0300

    model customer

--- djangoapp/store/models.py
+++ djangoapp/store/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Comprador(models.model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=200, null=True)
+    email=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
