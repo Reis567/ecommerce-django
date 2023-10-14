8809c4c82271903c4790c9bbff0a1ec05c614e75
Reis567 <matheusdosreis9@gmail.com>
2023-10-14 20:12:47 -0300

    card style fix

--- djangoapp/store/views.py
+++ djangoapp/store/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+from .models import *
+from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
 
 # Create your views here.
 def home(request):
-    context={}
-    return render(
-        request,
-        'store/home.html',
-        context
-    )
+    produtos = Produto.objects.all()
+
+    produtos_por_pagina = 6
+
+    paginator = Paginator(produtos, produtos_por_pagina)
+
+    # Obtenha o número da página a partir dos parâmetros da solicitação
+    page = request.GET.get('page')
+
+    try:
+        # Obtenha a página atual
+        produtos = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um número inteiro, exiba a primeira página
+        produtos = paginator.page(1)
+    except EmptyPage:
+        # Se a página estiver vazia (for maior que o número total de páginas), exiba a última página
+        produtos = paginator.page(paginator.num_pages)
+
+    context = {'produtos': produtos}
+
+    return render(request, 'store/home.html', context)
 
 def cart(request):
     context={}
