7c0516488ec052cc21a632f2b2b97389f20be5cf
Reis567 <matheusdosreis9@gmail.com>
2023-10-14 18:12:09 -0300

    model product

--- djangoapp/store/models.py
+++ djangoapp/store/models.py
@@ -9,4 +9,15 @@ class Comprador(models.model):
     email=models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.nome
+    
+
+
+class Produto(models.model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
