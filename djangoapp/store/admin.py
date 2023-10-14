67f4a0743b47112f04dfca96b25956ae47554d53
Reis567 <matheusdosreis9@gmail.com>
2023-10-14 18:56:47 -0300

    admin.site.register

--- djangoapp/store/admin.py
+++ djangoapp/store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from models import *
 # Register your models here.
+
+admin.site.register(Pedido)
+admin.site.register(Comprador)
+admin.site.register(EnderecoEnvio)
+admin.site.register(Produto)
+admin.site.register(ItemdePedido)
+
