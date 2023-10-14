e6a5baaae5bca3923d9d19dff7bc5f1a8d891557
Reis567 <matheusdosreis9@gmail.com>
2023-10-14 18:58:37 -0300

    models migration

--- djangoapp/store/admin.py
+++ djangoapp/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import *
+from .models import *
 # Register your models here.
 
 admin.site.register(Pedido)
@@ -7,4 +7,3 @@ admin.site.register(Comprador)
 admin.site.register(EnderecoEnvio)
 admin.site.register(Produto)
 admin.site.register(ItemdePedido)
-
