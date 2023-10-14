442e9e5b74a931af677de0b19115df68574a5e86
Reis567 <matheusdosreis9@gmail.com>
2023-10-14 19:13:47 -0300

    verbose name of models

--- djangoapp/store/models.py
+++ djangoapp/store/models.py
@@ -10,6 +10,10 @@ class Comprador(models.Model):
 
     def __str__(self):
         return self.nome
+
+    class Meta:
+        verbose_name = "Comprador"
+        verbose_name_plural = "Compradores"
     
 
 
@@ -22,6 +26,10 @@ class Produto(models.Model):
     def __str__(self):
         return self.nome
 
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+
 class Pedido(models.Model):
     comprador = models.ForeignKey(Comprador, on_delete=models.SET_NULL,null=True,blank=True )
     data_pedido = models.DateTimeField(auto_now_add=True)
@@ -31,20 +39,33 @@ class Pedido(models.Model):
     def __str__(self):
         return str(self.id)
 
+    class Meta:
+        verbose_name = "Pedido"
+        verbose_name_plural = "Pedidos"
+
 class ItemdePedido(models.Model):
     produto = models.ForeignKey(Produto, on_delete=models.SET_NULL, null=True)
     pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL,null=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     data_add = models.DateTimeField(auto_now_add=True)
 
-class EnderecoEnvio(models.Model):
-	comprador = models.ForeignKey(Comprador, on_delete=models.SET_NULL, null=True)
-	pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL, null=True)
-	endereco = models.CharField(max_length=200, null=False)
-	cidade = models.CharField(max_length=200, null=False)
-	estado = models.CharField(max_length=200, null=False)
-	cep = models.CharField(max_length=200, null=False)
-	date_add = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "Item de Pedido"
+        verbose_name_plural = "Itens de Pedido"
 
-	def __str__(self):
-		return self.endereco
+class EnderecoEnvio(models.Model):
+    comprador = models.ForeignKey(Comprador, on_delete=models.SET_NULL, null=True)
+    pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL, null=True)
+    endereco = models.CharField(max_length=200, null=False)
+    cidade = models.CharField(max_length=200, null=False)
+    estado = models.CharField(max_length=200, null=False)
+    cep = models.CharField(max_length=200, null=False)
+    date_add = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.endereco
+
+    class Meta:
+        verbose_name = "Endereço de Envio"
+        verbose_name_plural = "Endereços de Envio"
+
