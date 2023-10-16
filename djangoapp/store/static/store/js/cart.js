2e239504dd2ed016878f29eaa4c8a6591d379b3e
Reis567 <matheusdosreis9@gmail.com>
2023-10-16 18:13:19 -0300

    initial logic to send json values to django

--- djangoapp/store/static/store/js/cart.js
+++ djangoapp/store/static/store/js/cart.js
@@ -5,14 +5,33 @@ document.addEventListener("DOMContentLoaded", function () {
         updateBtns[i].addEventListener('click', function () {
             var productId = this.getAttribute('data-product');
             var action = this.getAttribute('data-action');
-            console.log('Id :', productId, 'Ação : ', action);
+            
             console.log('User :' ,user)
 
             if(user=="AnonymousUser"){
                 console.log('User is not authenticated')
             }else{
-                console.log('User is authenticated, sending data...')
+                updateUserOrder(productId, action)
             }
         });
+    }
+    function updateUserOrder(productId, action){
+        console.log('User authenticated')
+
+        var url = '/updateItem/'
+
+        fetch(url,{
+            method:'POST',
+            headers:{
+                'Content-Type':'aplication/json',
+            },
+            body:JSON.stringify({'productId':productId , 'action':action})
+        })
+        .then((response) =>{
+            return response.json();
+        })
+        .then((data)=>{
+            console.log('data : ', data)
+        })
     }
 });
