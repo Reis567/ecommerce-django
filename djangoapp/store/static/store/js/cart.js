40c10ad4ec52d21919c441a487a518b3b46063ba
Reis567 <matheusdosreis9@gmail.com>
2023-10-16 09:06:05 -0300

    function in js  to add items in cart

--- djangoapp/store/static/store/js/cart.js
+++ djangoapp/store/static/store/js/cart.js
@@ -1,1 +1,11 @@
-console.log('Botafogo botafogo campeao desde 1907')
+document.addEventListener("DOMContentLoaded", function () {
+    var updateBtns = document.getElementsByClassName('update-cart');
+
+    for (var i = 0; i < updateBtns.length; i++) {
+        updateBtns[i].addEventListener('click', function () {
+            var productId = this.getAttribute('data-product');
+            var action = this.getAttribute('data-action');
+            console.log('Id :', productId, 'Ação : ', action);
+        });
+    }
+});
