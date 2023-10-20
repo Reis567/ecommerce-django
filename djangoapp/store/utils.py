361e3e9fd8773260507a32f1cff6d7a89d189be0
Reis567 <matheusdosreis9@gmail.com>
2023-10-20 08:44:44 -0300

    separate cookie funcs on utils.py

---
+++ djangoapp/store/utils.py
@@ -0,0 +1,50 @@
+import json
+from .models import *
+
+def cookieCart(request):
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            print('New user')
+            cart= {}
+
+
+        print('Carrinho: ', cart)
+        items = []
+        pedido = {'get_cart_total':0,
+                  'get_cart_items':0,
+                  'shipping':False,
+                  }
+        itemsCarrinho = pedido['get_cart_items']
+
+        for i in cart :
+            try:
+                print(cart[i])
+                itemsCarrinho += cart[i]['quantidade']
+
+                produto = Produto.objects.get(id=i)
+                print(produto)
+                total = (produto.price * cart[i]['quantidade'])
+
+                pedido['get_cart_total'] += total
+                pedido['get_cart_items'] += cart[i]['quantidade']
+
+                item = {
+                    'produto':{
+                        'id':produto.id,
+                        'nome':produto.nome,
+                        'price':produto.price,
+                        'foto':produto.foto.url
+                    },
+                    'quantidade':cart[i]['quantidade'],
+                    'get_total':total
+                }
+                print(item)
+                items.append(item)
+
+                if produto.digital == False:
+                    pedido['shipping'] = True
+            except:
+                print('Item nao existe')
+    
+        return {'itemsCarrinho':itemsCarrinho,'pedido':pedido, 'items':items }
--- djangoapp/store/views.py
+++ djangoapp/store/views.py
@@ -7,6 +7,7 @@ import json
 import datetime
 from django.views.decorators.csrf import csrf_exempt
 from decimal import Decimal
+from .utils import *
 
 # Create your views here.
 def home(request):
@@ -55,50 +56,11 @@ def cart(request):
         items = pedido.itemdepedido_set.all()
         itemsCarrinho = pedido.get_cart_items
     else:
-        try:
-            cart = json.loads(request.COOKIES['cart'])
-        except:
-            print('New user')
-            cart= {}
+        cookieData = cookieCart(request)
 
-
-        print('Carrinho: ', cart)
-        items = []
-        pedido = {'get_cart_total':0,
-                  'get_cart_items':0,
-                  'shipping':False,
-                  }
-        itemsCarrinho = pedido['get_cart_items']
-
-        for i in cart :
-            try:
-                print(cart[i])
-                itemsCarrinho += cart[i]['quantidade']
-
-                produto = Produto.objects.get(id=i)
-                print(produto)
-                total = (produto.price * cart[i]['quantidade'])
-
-                pedido['get_cart_total'] += total
-                pedido['get_cart_items'] += cart[i]['quantidade']
-
-                item = {
-                    'produto':{
-                        'id':produto.id,
-                        'nome':produto.nome,
-                        'price':produto.price,
-                        'foto':produto.foto.url
-                    },
-                    'quantidade':cart[i]['quantidade'],
-                    'get_total':total
-                }
-                print(item)
-                items.append(item)
-
-                if produto.digital == False:
-                    pedido['shipping'] = True
-            except:
-                print('Item nao existe')
+        itemsCarrinho = cookieData['itemsCarrinho']
+        pedido = cookieData['pedido']
+        items = cookieData['items']
         
     context={'items':items,
              'pedido':pedido ,
