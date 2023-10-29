d8531a996831399cc007b0dfcb3aa53f3532678b
Reis567 <matheusdosreis9@gmail.com>
2023-10-29 17:22:19 -0300

    urls of password reset

--- djangoapp/store/urls.py
+++ djangoapp/store/urls.py
@@ -46,6 +46,20 @@ urlpatterns = [
     #store:favoritos
     path('favoritos/',FavoriteListView.as_view(),name='favoritos'),
 
+    #store:reset_password
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name ='reset_password'),
+
+    #store:password_reset_done
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
+
+    #store:password_reset_confirm
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+
+    #store:password_reset_complete
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
+
+    
+
 
 ]
 
