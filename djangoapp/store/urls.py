f682bc5f0222adaa17d0b69412223f34a4da7a8c
Reis567 <matheusdosreis9@gmail.com>
2023-10-29 17:23:50 -0300

    template name of reset password views

--- djangoapp/store/urls.py
+++ djangoapp/store/urls.py
@@ -47,16 +47,16 @@ urlpatterns = [
     path('favoritos/',FavoriteListView.as_view(),name='favoritos'),
 
     #store:reset_password
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name ='reset_password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "reset_password.html"), name ='reset_password'),
 
     #store:password_reset_done
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "password_reset_sent.html"), name ='password_reset_done'),
 
     #store:password_reset_confirm
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "password_reset_form.html"), name ='password_reset_confirm'),
 
     #store:password_reset_complete
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "password_reset_done.html"), name ='password_reset_complete'),
 
     
 
